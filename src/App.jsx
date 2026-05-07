c5f03448038fe465ead88b4fa2fde06ac5a16388
AbdulHannan18 <ah4334265@gmail.com>
2026-05-07 13:10:13 +0500

    Checking merge fail situation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div id="parent-div">
-      <div id="child-div">My jenkin Tesing</div>
+      <div id="child-div"></div>
       <div id="child-div-1">My jenkins Testing 1</div>
       <div>My jenkins Testing 2</div>
       <div>My jenkins Testing 3</div>
